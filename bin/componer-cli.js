ea442b743d6d6b61cbc799ee1d962c6773d484eb
ttang <tison.tang@morningstar.com>
2017-01-17 13:44:20 +0800

    update version and publish to npm

--- bin/componer-cli.js
+++ bin/componer-cli.js
@@ -334,28 +334,13 @@ _commander2.default.command("link [name]").description("(gulp) link local [name]
 	}
 });
 
-_commander2.default.command("remove <name>").alias("rm").description("remove a componout from componouts directory").action(function (name) {
+_commander2.default.command("remove <name>").alias("rm").description("(gulp) remove a componout from componouts directory").action(function (name) {
 	name = dashline(name);
 	check(name);
-
-	prompt("Are you sure to remove " + name + " componout? yes/No  ", function (choice) {
-		if (choice.toLowerCase() === "yes") {
-			if (exists(cwd + "/bower_components/" + name)) {
-				execute("cd " + cwd + " && bower unlink " + name);
-			}
-
-			if (exists(cwd + "/node_modules/" + name)) {
-				execute("cd " + cwd + " && npm unlink " + name);
-			}
-
-			execute("cd " + cwd + " && cd componouts && rm -rf " + name, function () {
-				log("Done! " + name + " has been deleted.", "done");
-			});
-
-			exit();
-		}
-	});
+	execute("cd " + cwd + " && gulp remove " + name);
 });
+
+// ----------------------------------------------------
 
 _commander2.default.command("pull <name> [params...]").description("clone/pull a componout from https://github.com/componer").option("-u, --url", "resgtry url").action(function (name, options, params) {
 	name = dashline(name);
