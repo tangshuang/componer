ce8cd5c7e48f62f0ebd551145ee2289de0678bd9
唐霜 <mail@tangshuang.net>
2017-03-01 21:52:17 +0800

    update preview: 1. ignore deps if there is no deps, 2. only reload css when style files changed, which will not reload page after modify, like inject

--- generator/gulp/tasks/preview.js
+++ generator/gulp/tasks/preview.js
@@ -1,7 +1,9 @@
 import {gulp, path, fs, args, log, config, exit, exists, clear, load, read, readJSON} from "../loader"
 import {hasComponout, dashlineName, camelName, runTask, getFileExt, setFileExt, StreamContent} from "../utils"
 
+import glob from "glob"
 import browsersync from "browser-sync"
+
 import webpack from "webpack"
 import webpackStream from "webpack-stream"
 import webpackConfig from "../drivers/webpack.config"
@@ -10,33 +12,33 @@ import sass from "gulp-sass"
 import sourcemap from "gulp-sourcemaps"
 
 gulp.task("preview", () => {
-	var arg = args.preview
-	var name = dashlineName(arg.name)
+	let arg = args.preview
+	let name = dashlineName(arg.name)
 
 	if(!hasComponout(name)) {
 		log(`${name} not exists.`, "error")
 		exit()
 	}
 
-	var componoutPath = path.join(config.paths.componouts, name)
-	var srcPath = path.join(componoutPath, "src")
+	let componoutPath = path.join(config.paths.componouts, name)
+	let srcPath = path.join(componoutPath, "src")
 
 	if(!exists(componoutPath + "/componer.config.js")) {
 		log("componer.config.js not exists.", "error")
 		exit()
 	}
 
-	var info = load(componoutPath + "/componer.config.js").preview
+	let info = load(componoutPath + "/componer.config.js").preview
 	if(!info) {
 		log("preview option in componer.config.js not found.", "error")
 		exit()
 	}
 
-	var indexfile = path.join(componoutPath, info.index)
-	var scriptfile = info.script ? path.join(componoutPath, info.script) : false
-	var stylefile = info.style ? path.join(componoutPath, info.style) : false
-	var serverfile = info.server ? path.join(componoutPath, info.server) : false
-	var tmpdir = info.tmpdir ? path.join(componoutPath, info.tmpdir) : path.join(componoutPath, ".preview_tmp")
+	let indexfile = path.join(componoutPath, info.index)
+	let scriptfile = info.script ? path.join(componoutPath, info.script) : false
+	let stylefile = info.style ? path.join(componoutPath, info.style) : false
+	let serverfile = info.server ? path.join(componoutPath, info.server) : false
+	let tmpdir = info.tmpdir ? path.join(componoutPath, info.tmpdir) : path.join(componoutPath, ".preview_tmp")
 
 	if(!exists(indexfile)) {
 		log("preview index file is not found.", "error")
@@ -49,19 +51,20 @@ gulp.task("preview", () => {
 	 * pre build dependencies vendors
 	 */
 
+	let bowerJson = path.join(componoutPath, "bower.json")
+ 	let pkgJson = path.join(componoutPath, "package.json")
+ 	let getDeps = function(pkgfile) {
+ 		if(!exists(pkgfile)) {
+ 			return []
+ 		}
+ 		let deps = readJSON(pkgfile).dependencies
+ 		return Object.keys(deps)
+ 	}
+ 	let deps = getDeps(bowerJson).concat(getDeps(pkgJson))
+
 	if(scriptfile && exists(scriptfile)) {
-		let bowerJson = path.join(componoutPath, "bower.json")
-	 	let pkgJson = path.join(componoutPath, "package.json")
-		let getDeps = function(pkgfile) {
-			if(!exists(pkgfile)) {
-				return []
-			}
-			let deps = readJSON(pkgfile).dependencies
-			return Object.keys(deps)
-		}
-		let deps = getDeps(bowerJson).concat(getDeps(pkgJson))
 		// create vendor bundle
-		webpack(webpackConfig({
+		if(deps.length > 0) webpack(webpackConfig({
 			entry: {
 				vendor: deps,
 			},
@@ -87,8 +90,8 @@ gulp.task("preview", () => {
 	 * create a bs server app
 	 */
 
-	var app = browsersync.create()
-	var middlewares = [
+	let app = browsersync.create()
+	let middlewares = [
 		{
 			route: "/",
 			handle: function (req, res, next) {
@@ -96,11 +99,16 @@ gulp.task("preview", () => {
 				gulp.src(indexfile)
 					.pipe(StreamContent(html => {
 						if(stylefile && exists(stylefile)) {
-							html = html.replace("<!--styles-->", `<link rel="stylesheet" href="${name}.css">`)
+							let partten = html.indexOf("<!--styles-->") > -1 ? "<!--styles-->" : "</head>"
+							html = html.replace(partten, `<link rel="stylesheet" href="${name}.css">`)
 						}
 						if(scriptfile && exists(scriptfile)) {
-							html = html.replace("<!--scripts-->", `<script src="${name}.js"></script>`)
-							html = html.replace("<!--vendors-->", `<script src="${name}.vendor.js"></script>`)
+							if(deps.length > 0) {
+								let partten = html.indexOf("<!--vendors-->") > -1 ? "<!--vendors-->" : "</body>"
+								html = html.replace(partten, `<script src="${name}.vendor.js"></script>`)
+							}
+							let partten = html.indexOf("<!--scripts-->") > -1 ? "<!--scripts-->" : "</body>"
+							html = html.replace(partten, `<script src="${name}.js"></script>`)
 						}
 						res.end(html)
 						return html
@@ -113,7 +121,7 @@ gulp.task("preview", () => {
 		middlewares.unshift({
 			route: `/${name}.css`,
 			handle: function (req, res, next) {
-				if(req.originalUrl !== `/${name}.css`) {
+				if(req.originalUrl !== `/${name}.css` && req.originalUrl.indexOf(`/${name}.css?`) === -1) {
 					next()
 					return
 				}
@@ -135,7 +143,7 @@ gulp.task("preview", () => {
 		middlewares.unshift({
 			route: `/${name}.js`,
 			handle: function (req, res, next) {
-				if(req.originalUrl !== `/${name}.js`) {
+				if(req.originalUrl !== `/${name}.js` && req.originalUrl.indexOf(`/${name}.js?`) === -1) {
 					next()
 					return
 				}
@@ -149,10 +157,10 @@ gulp.task("preview", () => {
 						},
 						devtool: "source-map",
 						plugins: [
-							new webpack.DllReferencePlugin({
+							deps.length > 0 ? new webpack.DllReferencePlugin({
 								context: tmpdir,
 								manifest: load(tmpdir + `/${name}.vendor.js.json`),
-							}),
+							}) : undefined,
 						],
 					})))
 					.pipe(StreamContent((content, filepath) => {
@@ -174,15 +182,44 @@ gulp.task("preview", () => {
 		}
 	}
 
-	var watchFiles = info.watchFiles
+	let watchFiles = info.watchFiles
 	if(typeof watchFiles === "string") {
-		watchFiles = path.join(componoutPath, watchFiles)
+		watchFiles = [path.join(componoutPath, watchFiles)]
 	}
 	else if(watchFiles instanceof Array) {
 		watchFiles = watchFiles.map(item => path.join(componoutPath, item))
 	}
 	else {
 		watchFiles = []
+	}
+
+	if(watchFiles.length > 0) {
+		// only reload css files on page
+		let styleWatchFiles = []
+		let otherWatchFiles = []
+
+		watchFiles.forEach(item => {
+			let ext = getFileExt(item)
+			if(ext === ".scss" || ext === ".css") {
+				styleWatchFiles.push(item);
+			}
+			else {
+				// use glob to find out style files in sub directory
+				if(item.indexOf("*") > -1) glob.sync(item).forEach(_item => {
+					let _ext = getFileExt(_item)
+					if(_ext === ".scss" || _ext === ".css") {
+						styleWatchFiles.push(_item)
+					}
+				})
+				// put not style files into otherWatchFiles
+				otherWatchFiles.push(item)
+			}
+		})
+
+		// except style files
+		watchFiles = otherWatchFiles.concat([`!${componoutPath}/**/*.scss`, `!${componoutPath}/**/*.css`])
+		// watch style files by gulp, and reload css files after style files changed
+		gulp.watch(styleWatchFiles, event => app.reload("*.css"))
 	}
 
 	app.init({
