57275e9a83b0c01e9f7591114e5bb98e7763bb87
ttang <tison.tang@morningstar.com>
2017-03-02 10:03:44 +0800

    add .jshint to keep code style

--- generator/gulp/tasks/preview.js
+++ generator/gulp/tasks/preview.js
@@ -222,7 +222,9 @@ gulp.task("preview", () => {
 		// except style files
 		watchFiles = otherWatchFiles.concat([`!${componoutPath}/**/*.scss`, `!${componoutPath}/**/*.css`])
 		// watch style files by gulp, and reload css files after style files changed
-		gulp.watch(styleWatchFiles, event => if(event.type === "changed") app.reload("*.css"))
+		gulp.watch(styleWatchFiles, event => {
+			if(event.type === "changed") app.reload("*.css")
+		})
 	}
 
 	app.init({
