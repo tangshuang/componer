3c84c952acb77ee23da8f8d59c81cecc924ebc66
ttang <tison.tang@morningstar.com>
2017-03-03 13:33:10 +0800

    update list task

--- generator/gulp/tasks/list.js
+++ generator/gulp/tasks/list.js
@@ -1,16 +1,14 @@
-import {gulp, fs, config, log, logger} from "../loader"
-import {getComponouts, strPadRight} from "../utils"
+import {gulp, fs, config, log} from '../loader'
+import {getComponouts, strPadRight} from '../utils'
+import logger from 'process.logger'
 
-
-function listComponouts() {
+gulp.task('list', () => {
 	var componouts = getComponouts()
-	log(strPadRight(`======================= You have ${componouts.length} componouts: ==============`, '=', 70), "help")
-	logger.put(strPadRight("name", " ", 15)).put("\t" + strPadRight("version", " ", 10)).put("\ttype").print()
-	log("----------------------------------------------------------------------")
+	log(strPadRight(`======================= You have ${componouts.length} componouts: ==============`, '=', 70), 'help')
+	logger.put(strPadRight('name', ' ', 15)).put('\t' + strPadRight('version', ' ', 10)).put('\ttype').print()
+	log('----------------------------------------------------------------------')
 	componouts.forEach(item => {
-		logger.put(strPadRight(item.name, " ", 15), {color: "cyan"}).put("\t" + strPadRight(item.version, " ", 10)).put("\t" + item.type).print()
+		logger.put(strPadRight(item.name, ' ', 15), {color: 'cyan'}).put('\t' + strPadRight(item.version, ' ', 10)).put('\t' + item.type).print()
 	})
-	log("======================================================================", "help")
-}
-
-gulp.task("list", listComponouts)
+	log('======================================================================', 'help')
+})
