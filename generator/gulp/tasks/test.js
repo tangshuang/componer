71168814b9d1840b661f10d07a83d2943ee923e7
ttang <tison.tang@morningstar.com>
2017-03-03 13:44:56 +0800

    update test task

--- generator/gulp/tasks/test.js
+++ generator/gulp/tasks/test.js
@@ -1,53 +1,53 @@
-import {gulp, path, fs, args, log, config, exit, exists, load} from "../loader"
-import {hasComponout, dashlineName, runTask} from "../utils"
+import {gulp, path, fs, args, log, config, exit, exists, scandir, mkdir, load} from '../loader'
+import {hasComponout, dashName, run} from '../utils'
 
-import karma from "gulp-karma-runner"
-import jasmine from "gulp-jasmine-node"
-import karmaConfig from "../drivers/karma.config"
+import karma from 'gulp-karma-runner'
+import jasmine from 'gulp-jasmine-node'
+import karmaConfig from '../drivers/karma.config'
 
-gulp.task("test", () => {
+gulp.task('test', () => {
 	var arg = args.test
 	var debug = arg.debug
 	var browser = arg.browser
 	var componoutsPath = config.paths.componouts
 
 	if(arg.name === undefined) {
-		fs.readdirSync(componoutsPath).forEach(item => {
-			runTask("test", {
+		scandir(componoutsPath).forEach(item => {
+			run('test', {
 				name: item,
-				browser: "PhantomJS"
+				browser: 'PhantomJS'
 			})
 		})
 		return
 	}
 
-	var name = dashlineName(arg.name)
+	var name = dashName(arg.name)
 	if(!hasComponout(name)) {
-		log(`${name} not exists.`, "error")
+		log(`${name} not exists.`, 'error')
 		exit()
 	}
 
 	var componoutPath = path.join(config.paths.componouts, name)
-	var srcPath = path.join(componoutPath, "src")
+	var srcPath = path.join(componoutPath, 'src')
 
-	if(!exists(componoutPath + "/componer.config.js")) {
-		log("componer.config.js not exists.", "error")
+	if(!exists(componoutPath + '/componer.config.js')) {
+		log('componer.config.js not exists.', 'error')
 		exit()
 	}
 
-	var info = load(componoutPath + "/componer.config.js").test
+	var info = load(componoutPath + '/componer.config.js').test
 	if(!info) {
-		log("test option in componer.config.js not found.", "error")
+		log('test option in componer.config.js not found.', 'error')
 		exit()
 	}
 
 	if(!info.entry) {
-		log("test.entry option in componer.config.js not found.", "error")
+		log('test.entry option in componer.config.js not found.', 'error')
 		exit()
 	}
 	var entryfile = path.join(componoutPath, info.entry)
 	if(!exists(entryfile)) {
-		log(`test entry file not found.`, "error")
+		log(`test entry file not found.`, 'error')
 		exit()
 	}
 
@@ -55,11 +55,11 @@ gulp.task("test", () => {
 	/**
 	 * if it is a npm package, run test with jasmine-node
 	 */
-	if(info.browsers === "Terminal") {
+	if(info.browsers === 'Terminal') {
 		return gulp.src(entryfile).pipe(jasmine({
 			timeout: 10000,
 			includeStackTrace: false,
-			color: process.argv.indexOf("--color")
+			color: process.argv.indexOf('--color')
 		}))
 	}
 
@@ -70,18 +70,18 @@ gulp.task("test", () => {
 
 	var reportersDir = info.reporters
 	if(!reportersDir) {
-		log(`test.reporters option is not correct in your componer.json.`, "error")
+		log(`test.reporters option is not correct in your componer.json.`, 'error')
 		exit()
 	}
 
 	var reportersPath = path.join(componoutPath, reportersDir)
 	if(!exists(reportersPath)) {
-		fs.mkdir(reportersPath)
+		mkdir(reportersPath)
 	}
 
 	var preprocessors = {}
-	preprocessors[componoutPath + "/**/*.js"] = ["webpack", "sourcemap"]
-	preprocessors[componoutPath + "/**/*.scss"] = ["scss"]
+	preprocessors[componoutPath + '/**/*.js'] = ['webpack', 'sourcemap']
+	preprocessors[componoutPath + '/**/*.scss'] = ['scss']
 
 	var karmaSettings = {
 			singleRun: debug !== undefined ? !debug : !info.debug,
@@ -90,7 +90,7 @@ gulp.task("test", () => {
 			coverageReporter: {
 				reporters: [
 					{
-						type: "html",
+						type: 'html',
 						dir: reportersPath,
 					},
 				],
@@ -103,8 +103,8 @@ gulp.task("test", () => {
 
 	return gulp.src(entryfile)
 		.pipe(karma.server(karmaConfig(karmaSettings)))
-		.on("end", () => {
-			log("Reporters ware created in componouts/" + name + "/" + reportersDir, "help")
+		.on('end', () => {
+			log('Reporters ware created in componouts/' + name + '/' + reportersDir, 'help')
 			exit()
 		})
 
