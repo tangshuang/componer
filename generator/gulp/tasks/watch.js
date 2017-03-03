cc00b288daed569d7d8c283848055d0eb087bd2a
ttang <tison.tang@morningstar.com>
2017-03-03 13:35:30 +0800

    update watch task

--- generator/gulp/tasks/watch.js
+++ generator/gulp/tasks/watch.js
@@ -1,48 +1,48 @@
-import {gulp, path, fs, args, log, config, read} from "../loader"
-import {hasComponout, dashlineName, runTask} from "../utils"
+import {gulp, path, fs, args, log, config, read, scandir} from '../loader'
+import {hasComponout, dashName, run} from '../utils'
 
-gulp.task("watch", () => {
+gulp.task('watch', () => {
 	var arg = args.watch
 	var entryfiles = []
 	var componoutsPath = config.paths.componouts
 
 	if(arg.name === undefined) {
-		fs.readdirSync(componoutsPath).forEach(item => {
-			let srcPath = path.join(componoutsPath, item, "src")
-			entryfiles.push(srcPath + "/**/*")
+		scandir(componoutsPath).forEach(item => {
+			let srcPath = path.join(componoutsPath, item, 'src')
+			entryfiles.push(srcPath + '/**/*')
 		})
 	}
 	else {
-		var name = dashlineName(arg.name)
+		var name = dashName(arg.name)
 		if(!hasComponout(name)) {
-			log(`${name} not exists.`, "error")
+			log(`${name} not exists.`, 'error')
 			exit()
 		}
 
-		var srcPath = path.join(componoutsPath, name, "src")
-		entryfiles.push(srcPath + "/**/*")
+		var srcPath = path.join(componoutsPath, name, 'src')
+		entryfiles.push(srcPath + '/**/*')
 	}
 
-	log("Watching, when code changed, componer will build it automaticly...", "help")
+	log('Watching, when code changed, componer will build it automaticly...', 'help')
 
 	let contents = {}
 	gulp.watch(entryfiles, event => {
-		if(event.type !== "changed") {
+		if(event.type !== 'changed') {
 			return
 		}
-		
+
 		// if file content not changed, do not run build task
 		let file = event.path
 		let content = read(file)
 		if(contents[file] && contents[file] === content) return
 		contents[file] = content
 
-		log(`${event.path} was ${event.type}, building...`, "help")
+		log(`${event.path} was ${event.type}, building...`, 'help')
 
-		let relativePath = path.relative(componoutsPath, file).replace(/\\/g, "/")
-		let componoutName = relativePath.split("/")[0]
+		let relativePath = path.relative(componoutsPath, file).replace(/\\/g, '/')
+		let componoutName = relativePath.split('/')[0]
 
-		runTask("build", {
+		run('build', {
 			name: componoutName
 		})
 	})
