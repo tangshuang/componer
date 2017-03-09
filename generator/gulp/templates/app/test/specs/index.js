66167b80d1c0c7865c041ef3efe0ded77d6bc4da
ttang <tison.tang@morningstar.com>
2017-03-09 13:12:29 +0800

    update instance name

--- generator/gulp/templates/app/test/specs/index.js
+++ generator/gulp/templates/app/test/specs/index.js
@@ -2,7 +2,7 @@ import {{ComponoutName}} from '../../src/script/{{componout-name}}'
 
 describe('{{Componout Name}}', () => {
     it('test', () => {
-      let ${{componoutName}} = new {{ComponoutName}}()
-      expect(${{componoutName}}).toBeTruthy()
+      let {{componoutName}} = new {{ComponoutName}}()
+      expect({{componoutName}}).toBeTruthy()
     })
 })
--- generator/gulp/templates/component/preview/index.js
+++ generator/gulp/templates/component/preview/index.js
@@ -1,6 +1,6 @@
 import {{ComponoutName}} from '../src/script/{{componout-name}}'
 
-var ${{componoutName}} = new {{ComponoutName}}()
-var type = ${{componoutName}}.toString()
+var {{componoutName}} = new {{ComponoutName}}()
+var type = {{componoutName}}.toString()
 
 console.log(type)
